9df3346eec7b1d885583ae85453043c46973ec6a
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-08-13 02:20:28 +0100

    feat FindClientUseCase

---
+++ src/modules/client-adm/usecase/find-client/find-client.usecase.spec.ts
@@ -0,0 +1,35 @@
+import Id from '../../../@shared/domain/value-object/id.value-object'
+import Client from '../../domain/client.entity'
+import FindClientUseCase from './find-client.usecase'
+
+const client = new Client({
+  id: new Id('1'),
+  name: 'John Doe',
+  email: 'x@x.com',
+  address: 'Address 1'
+})
+
+const MockRepository = () => {
+  return {
+    add: jest.fn(),
+    find: jest.fn().mockReturnValue(Promise.resolve(client))
+  }
+}
+
+describe('Find Client usecase unit test', () => {
+  it('Should find a client', async () => {
+    const clientRepository = MockRepository()
+    const usecase = new FindClientUseCase(clientRepository)
+    const input = { id: '1' }
+    const output = await usecase.execute(input)
+    expect(clientRepository.find).toHaveBeenCalled()
+    expect(output).toEqual({
+      id: client.id.id,
+      name: client.name,
+      email: client.email,
+      address: client.address,
+      createdAt: client.createdAt,
+      updatedAt: client.updatedAt
+    })
+  })
+})
---
+++ src/modules/client-adm/usecase/find-client/find-client.usecase.ts
@@ -0,0 +1,21 @@
+import UseCaseInterface from '../../../@shared/usecase/use-case.interface'
+import ClientGateway from '../../gateway/client.gateway'
+import { FindClientInputDto, FindClientOutputDto } from './find-client.dto'
+
+export default class FindClientUseCase implements UseCaseInterface {
+  constructor (
+    private readonly _clientRepository: ClientGateway
+  ) {}
+
+  async execute (input: FindClientInputDto): Promise<FindClientOutputDto> {
+    const client = await this._clientRepository.find(input.id)
+    return {
+      id: client.id.id,
+      name: client.name,
+      email: client.email,
+      address: client.address,
+      createdAt: client.createdAt,
+      updatedAt: client.updatedAt
+    }
+  }
+}
