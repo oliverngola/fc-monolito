d18a855b23e6b3db9f70f34b32ef6f903698bbb1
Oliveira Ngola <oliveiraernestongola123@gmail.com>
2023-08-13 23:44:54 +0100

    feat ClientAdmFacade

--- src/modules/client-adm/usecase/add-client/add-client.usecase.ts
+++ src/modules/client-adm/usecase/add-client/add-client.usecase.ts
@@ -1,3 +1,4 @@
+import Id from '../../../@shared/domain/value-object/id.value-object'
 import UseCaseInterface from '../../../@shared/usecase/use-case.interface'
 import Client from '../../domain/client.entity'
 import ClientGateway from '../../gateway/client.gateway'
@@ -10,6 +11,7 @@ export default class AddClientUseCase implements UseCaseInterface {
 
   async execute (input: AddClientInputDto): Promise<AddClientOutputDto> {
     const client = new Client({
+      id: new Id(input.id) || new Id(),
       name: input.name,
       email: input.email,
       address: input.address
